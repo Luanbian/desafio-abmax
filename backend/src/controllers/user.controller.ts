6db1f694ed3a241bf4623f307d3041176b1700ef
Luan Bianchini <luanbianchine1@gmail.com>
2023-05-16 15:05:56 -0300

    feat: add error handling in register method

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -4,11 +4,18 @@ export class UserController implements IUserController{
     constructor(private readonly userDatabase: IUserDatabase){}
 
     public register = async (register: inputNewUser): Promise<IHttpsResponse> => {
-        const newRegister = await this.userDatabase.insertUser(register);
-        return {
-            message: 'created',
-            statusCode: 200,
-            data: newRegister
+        try {
+            const newRegister = await this.userDatabase.insertUser(register);
+            return {
+                message: 'created',
+                statusCode: 200,
+                data: newRegister
+            }
+        } catch (error) {
+            return {
+                message: error.message,
+                statusCode: 400
+            }
         }
     }
 
--- backend/src/database/user.database.ts
+++ backend/src/database/user.database.ts
@@ -4,13 +4,15 @@ import knex from "../config/database";
 export class UserDatabase implements IUserDatabase {
     public dbName = 'users'
     async insertUser(register: inputNewUser) {
+        const existingUser = await knex.select('email').from(this.dbName).where('email', register.email);
+        if(existingUser.length != 0) return Promise.reject({message: 'Usuário já existe'});
         const result = await knex(this.dbName).insert(register);
         return result;
     }
 
     async login(login: inputLogin) {
         const existingUser = await knex.select('email', 'password').from(this.dbName).where(login);
-        if(existingUser.length != 0) return existingUser
-        return Promise.reject({message: 'Usuário não encontrado'})
+        if(existingUser.length == 0) return Promise.reject({message: 'Usuário não encontrado'});
+        return existingUser;
     }
 }
