e281857039708929a6bb823cc7c0d6299b46bce7
Luan Bianchini <luanbianchine1@gmail.com>
2023-05-16 13:30:34 -0300

    feat: sending login body to backend using axios and cors

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { router } from './src/routes/routes';
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(router)
 app.listen({port: 3030}, () => {
     console.log('Server listen at 3030')
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,7 +1,27 @@
 import { Card, CardHeader, Heading, CardBody, InputGroup, Input, InputLeftAddon, ButtonGroup ,Button } from '@chakra-ui/react'
+import { useRef } from 'react'
+import { baseURL } from '../api/api'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 export default function Login() {
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    const handleLogin = () => {
+        if(emailRef.current && passwordRef.current) {
+            const inputEmailValue = emailRef.current.value;
+            const inputPasswordValue = passwordRef.current.value;
+            if(inputEmailValue.length > 3 && inputPasswordValue.length > 3) {
+                const loginRequest = {
+                    email: inputEmailValue,
+                    password: inputPasswordValue
+                }
+                axios.post(`${baseURL}/login`, loginRequest).then(response => console.log(response));
+            }
+        }
+    }
+
     return (
         <Card maxW='sm'>
             <CardHeader>
@@ -10,15 +30,15 @@ export default function Login() {
             <CardBody>
                 <InputGroup>
                     <InputLeftAddon children='email'/>
-                    <Input placeholder='email' variant='flushed'/>
+                    <Input placeholder='email' variant='flushed' ref={emailRef}/>
                 </InputGroup>
                 <InputGroup>
                     <InputLeftAddon children='senha'/>
-                    <Input placeholder='senha' variant='flushed'/>
+                    <Input placeholder='senha' variant='flushed' ref={passwordRef}/>
                 </InputGroup>
             </CardBody>
             <ButtonGroup spacing='2'>
-                <Button variant='solid' colorScheme='blue'>
+                <Button variant='solid' colorScheme='blue' onClick={handleLogin}>
                     Entrar
                 </Button>
                 <Button variant='ghost' colorScheme='blue'>
